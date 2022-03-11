2084336d0636b2cd7db1f0bdf7ac90f205621af8
Baptiste Riffard <briffard@c1r1p3.hive.fi>
2022-03-11 15:38:05 +0200

    UPDATE V2 || BAD COMMAND PROBLEM

--- example-agent-v2/main.c
+++ example-agent-v2/main.c
@@ -4,141 +4,241 @@
 #include <stdio.h>
 #include "agent.h"
 
-int		is_col_even(int col)
+int		is_empty(int dir, agent_info_t info);
+
+int		is_up(int col, int player)
 {
 	int	index = 0;
-	int even[] = {2, 8, 14, 20, 26, 28};
-	while(index < 6)
+	int upP0[] = {2, 8, 14, 20, 26};
+	int upP1[] = {27, 21, 15, 9, 3};
+
+	while(index < 5)
 	{
-		if (col == even[index])
-			return 1;
+		if (player == 0)
+		{
+			if (col == upP0[index])
+				return 1;
+		}
+		else if (player == 1)
+		{
+			if (col == upP1[index])
+				return 1;
+		}
 		index++;
 	}
 	return 0;
 }
 
-int is_col_odd(int col)
+int is_down(int col, int player)
 {
 	int	index = 0;
-	int odd[] = {1, 5, 11, 17, 23, 27, 29};
-	while (index < 7)
+	int downP0[] = {5, 11, 17, 23, 29};
+	int downP1[] = {24, 18, 12, 6, 0};
+	
+	while (index < 5)
 	{
-		if (col == odd[index])
-			return 1;
+		if (player == 0)
+		{	if (col == downP0[index])
+				return 1;
+		}
+		else if (player == 1)
+		{	if (col == downP1[index])
+				return 1;
+		}
 		index++;
 	}
 	return 0;
 }
 
-int		bee_spy(agent_info_t info, char side)
+int		bee_spy(agent_info_t info)
 {
-	switch(side)
-	{
-		case 'L':
-			if (info.col == 26 && info.row != 11)
-				return N;
-			else if (info.col == 29 && info.row !=14)
-				return S;
-			else
-			{
-				if(info.row == 15)
-					return W;
-				else 
-					return E;
-			}
-			break;
-		case 'R':
-			if (info.col == 3 && info.row != 11)
-				return N;
-			else if (info.col == 0 && info.row !=14)
-				return S;
-			else
-			{
-				if(info.row == 15)
-					return E;
-				else 
-					return W;
-			}
-			break;
-		default:
-			return (rand() % 8);
+	if (info.player == 0)
+	{	
+		if (info.col == 26 && info.row != 11)
+			return is_empty(N, info);
+		else if (info.col == 29 && info.row !=14)
+			return is_empty(S, info);
+		else
+		{
+			if(info.row == 15)
+				return is_empty(W, info);
+			else 
+				return is_empty(E, info);
+		}
 	}
+	else if (info.player == 1)
+	{
+		if (info.col == 3 && info.row != 11)
+			return is_empty(N, info);
+		else if (info.col == 0 && info.row !=14)
+			return is_empty(S, info);
+		else
+		{
+			if(info.row == 15)
+				return is_empty(E, info);
+			else 
+				return is_empty(W, info);
+		}
+	}
+		return (0);
+}
+
+int		go_around( int dir, agent_info_t info)
+{
+	if (info.bee == 3 || info.bee == 4)
+	{
+		if (dir == N)
+			return is_empty(rand() % 8, info);
+		return(is_empty(dir - 1, info));
+	}
+	else
+	{
+		if (dir == NE)
+			return is_empty(rand() % 8, info);
+		return (is_empty(dir + 1, info));
+	}
+}
+
+int		is_empty(int dir, agent_info_t info)
+{
+	coords_t center = {VIEW_DISTANCE, VIEW_DISTANCE};
+	coords_t coords = direction_to_coords(center, dir);
+	cell_t next_position = info.cells[coords.row][coords.col];
+	if(next_position == EMPTY)
+		return dir;
+	return go_around(dir, info);
 }
 
 int		looking_for_flower(agent_info_t info)
 {
-		if(info.bee == 0 || info.bee == 1)
-			{
-				if (info.bee == 1 && info.col < 15)
-					return E;
-				if(is_col_even(info.col) == 1 && info.row != 0)
-						return N;
-				else if (is_col_odd(info.col) == 1 && info.row != 11)
-						return S;
-				else
-				{
-					if (info.player == 0)
-						return E;
-					if(info.player == 1)
-						return W;
-				}
-			}
-			else if(info.bee == 3 || info.bee == 4)
-			{
-				if (info.player == 0)
-					if (info.bee  == 3 && info.col < 15)
-						return E;
-				if(info.player == 1 && info.col > 15 && info.bee == 3)
-						return W;
-				if(is_col_even(info.col) == 1 && info.row != 24)
-						return N;
-				else if (is_col_odd(info.col) == 1 && info.row != 15)
-						return S;
-				else
-				{
-					if (info.player == 0)
-						return E;
-					if (info.player == 1)
-						return W;
-				}
-			}
-/*	if (side == 'R')
+	if (info.player == 0)
 	{
-			if(info.bee == 0 || info.bee == 1)
+		if (info.bee == 0)
+		{
+			if (is_up(info.col, info.player) == 1 && info.row != 1)
 			{
-				if (info.bee == 1 && info.col > 15)
-					return W;
-				if(is_col_even(info.col) == 1 && info.row != 0)
-					return N;
-				else if (is_col_odd(info.col) == 1 && info.row != 11)
-					return S;
-				else
-					return W;
+				return is_empty(N, info);
 			}
-			else if(info.bee == 3 || info.bee == 4)
-			{
-				if (info.bee  == 3 && info.col > 15)
-					return W;
-				if(is_col_even(info.col) == 1 && info.row != 24)
-					return N;
-				else if (is_col_odd(info.col) == 1 && info.row != 15)
-					return S;
-				else
-					return W;
-			}
-	}*/
-	return (rand() % 8);
+			if (is_down(info.col, info.player) == 1 && info.row != 10)
+				return is_empty(S, info);
+			else 
+				return is_empty(E, info);
+		}
+		else if (info.bee == 1)
+		{
+			if(info.col < 15)
+				return is_empty(E, info);
+			if (is_up(info.col, info.player) == 1 && info.row != 1)
+				return is_empty(N, info);
+			else if (is_down(info.col, info.player) == 1 && info.row != 10)
+				return is_empty(S, info);
+			else 
+				return is_empty(E, info);
+		}
+		else if(info.bee == 2)
+		{
+			if (info.col == 25 && info.row != 11)
+				return is_empty(N, info);
+			else if (info. col == 30 && info.row != 14)
+				return is_empty(S, info);
+			else
+				{
+					if(info.row == 14)
+						return is_empty(W, info);
+					else
+						return is_empty(E, info);
+				}
+		}
+		else if (info.bee == 3)
+		{
+			if(info.col < 15)
+				return is_empty(E, info);
+			if (is_up(info.col, info.player) == 1 && info.row != 23)
+				return is_empty(S, info);
+			else if (is_down(info.col, info.player) == 1 && info.row != 12)
+				return is_empty(N, info);
+			else 
+				return is_empty(E, info);
+		}
+		else if (info.bee == 4)
+		{
+			if (is_up(info.col, info.player) == 1 && info.row != 23)
+				return is_empty(S, info);
+			else if (is_down(info.col, info.player) == 1 && info.row != 12)
+				return is_empty(N, info);
+			else 
+				return is_empty(E, info);
+		}
+	}
+	else if (info.player == 1)
+	{
+		if (info.bee == 0)
+		{
+			if (is_up(info.col, info.player) == 1 && info.row != 1)
+				return is_empty(N, info);
+			else if (is_down(info.col, info.player) == 1 && info.row != 10)
+				return is_empty(S, info);
+			else 
+				return is_empty(W, info);
+		}
+		else if (info.bee == 1)
+		{
+			if(info.col > 15)
+				return is_empty(W, info);
+			if (is_up(info.col, info.player) == 1 && info.row != 1)
+				return is_empty(N, info);
+			else if (is_down(info.col, info.player) == 1 && info.row != 10)
+				return is_empty(S, info);
+			else 
+				return is_empty(W, info);
+		}
+		else if(info.bee == 2)
+		{
+			if (info.col == 4 && info.row != 10)
+				return is_empty(N, info);
+			else if (info. col == 0 && info.row != 15)
+				return is_empty(S, info);
+			else
+				{
+					if(info.row == 14)
+						return is_empty(W, info);
+					else
+						return is_empty(E, info);
+				}
+		}
+		else if (info.bee == 3)
+		{
+			if(info.col > 15)
+				return is_empty(W, info);
+			if (is_up(info.col, info.player) == 1 && info.row != 23)
+				return is_empty(S, info);
+			else if (is_down(info.col, info.player) == 1 && info.row != 12)
+				return is_empty(N, info);
+			else 
+				return is_empty(W, info);
+		}
+		else if (info.bee == 4)
+		{
+			if (is_up(info.col, info.player) == 1 && info.row != 23)
+				return is_empty(S, info);
+			else if (is_down(info.col, info.player) == 1 && info.row != 12)
+				return is_empty(N, info);
+			else 
+				return is_empty(W, info);
+		}
+	}
+	return (0);
 }
 
 
-int		dispacht_bee(int bee, agent_info_t info, char side)
+int		dispacht_bee(int bee, agent_info_t info)
 {
 	if (bee == 0 || bee == 1 || bee == 3 || bee == 4)
 	{
 		return looking_for_flower(info);
 	}
 	else
-		return bee_spy(info, side);
+		return bee_spy(info);
 
 }
 
@@ -182,7 +282,7 @@ int find_neighbour(agent_info_t info, cell_t type)
 
     return -1;
 }
-
+/*
 int	go_around(int bee_position)
 {
 	if(bee_position == N)
@@ -196,6 +296,7 @@ int	go_around(int bee_position)
 	else
 		return (rand() % 8);
 }
+
 
 int bee_on_the_way(agent_info_t info)
 {
@@ -215,6 +316,7 @@ int	flower_on_the_way(agent_info_t info)
 		return go_around(flower_dir);
 	return (-1);
 }
+
 
 command_t	 wall_on_the_way(agent_info_t info)
 {
@@ -255,24 +357,21 @@ command_t	attack_other_bee(agent_info_t info)
 		}
 	}
 }
-
+*/
 command_t think(agent_info_t info)
 {
 	int 	hive_col;
 	int 	hive_row;
-	char	side;
 
 	if (info.player == 1)
 	{
-		hive_col = 28;
+		hive_col = 27;
 		hive_row = 12;
-		side = 'R';
 	}
 	else if (info.player == 0)
 	{
 		hive_col = 2;
 		hive_row = 12;
-		side = 'L';
 	}
 
 	cell_t bee = info.cells[VIEW_DISTANCE][VIEW_DISTANCE];
@@ -289,28 +388,28 @@ command_t think(agent_info_t info)
             };
         }
 		else
-		{
-			//if(bee_on_the_way(info) >= 0)
-			//{
-			//	return(command_t){
-			//		.action = MOVE,
-			//		.direction = bee_on_the_way(info)
-			//	};
-			//}
-			//else if(flower_on_the_way(info) >= 0)
-			//{
-			//	return (command_t){
-			//		.action = MOVE,
-			//		.direction = flower_on_the_way(info)
-			//	};
-			//}
-			//else
-			//	{
+		{/*
+			if(bee_on_the_way(info) >= 0)
+			{
+				return(command_t){
+					.action = MOVE,
+					.direction = bee_on_the_way(info)
+				};
+			}
+			else if(flower_on_the_way(info) >= 0)
+			{
+				return (command_t){
+					.action = MOVE,
+					.direction = flower_on_the_way(info)
+				};
+			}
+			else
+		*/		//{
 					return (command_t){
 						.action = MOVE,
-						.direction = go_home(info, hive_row, hive_col)
+						.direction = is_empty(go_home(info, hive_row, hive_col),info)
 					};
-			//	}
+				//}
 		}
     }
     else
@@ -324,7 +423,7 @@ command_t think(agent_info_t info)
             };
         }
     }
-	if (bee_on_the_way(info) >= 0 && info.turn == 6)
+/*	if (bee_on_the_way(info) >= 0 && info.turn > 6)
 	{
 		return(command_t){
 			.action = MOVE,
@@ -332,12 +431,12 @@ command_t think(agent_info_t info)
 		};
 	}
 	else
-	{
+*/	//{
     	return (command_t) {
         .action = MOVE,
-        .direction = dispacht_bee(info.bee, info, side)
+        .direction = dispacht_bee(info.bee, info)
     	};
-	}
+	//}
 }
 
 
@@ -355,7 +454,7 @@ int main(int argc, char **argv)
 
     char *host = argv[1];
     int port = atoi(argv[2]);
-    char *team_name = "Les muscles";
+    char *team_name = "Framboisier";
 
     agent_main(host, port, team_name, think);
 }
